bdbd39372e2bd059f1f5e85f93092da70c8bd05c
Kris De Volder <kdevolder@pivotal.io>
2016-06-04 19:57:59 -0700

    Cleanup whitespace

--- src/test/java/com/github/kdvolder/tttree/test/TTTreeTest.java
+++ src/test/java/com/github/kdvolder/tttree/test/TTTreeTest.java
@@ -4,8 +4,6 @@ import com.github.kdvolder.tttree.TTTree;
 
 public class TTTreeTest extends AbstractMapTestTemplate {
 
-
-
 	@Override
 	protected <K extends Comparable<K>, V> MutableMap<K, V> createEmptyMap() {
 		return MutableMap.<K,V>from(TTTree.empty());
